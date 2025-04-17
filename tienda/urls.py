55d37de4a37e94040534c0e764b34fba17510b90
ksan240 <carlos.santoyo@educa.madrid.org>
2025-04-17 21:25:35 +0200

    3 navbar

--- tienda/urls.py
+++ tienda/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('hola/', views.index, name='index'),
+    path('ferrari/', views.ferrari, name='ferrari'),
+    path('pagani/', views.pagani, name='pagani'),
+    path('lamborghini/', views.lamborghini, name='lamborghini'),
+    path('mclaren/', views.mclaren, name='mclaren'),
+    path('bmw/', views.bmw, name='bmw'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def ferrari(request):
+    return render(request, 'ferrari.html')
+
+def pagani(request):
+    return render(request, 'pagani.html')
+
+def lamborghini(request):
+    return render(request, 'lamborghini.html')
+
+def mclaren(request):
+    return render(request, 'mclaren.html')
+
+def bmw(request):
+    return render(request, 'bmw.html')
